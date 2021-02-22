dd801e8fdbf8be133d062282a015c6893d145b3a
maraesa100 <siobhanmckenzie90@gmail.com>
2021-02-22 10:50:12 +0000

    Remove unneeded module

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import weatherReducer from '../features/weather/weatherSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     weather: weatherReducer
   }
 })
